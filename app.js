93723bad2a694043eda24766a0a4bd61982c7890
Devansh Shah <shahdevansh2000@gmail.com>
2018-11-10 22:04:14 +0530

    Finished everything except cancel booking

--- app.js
+++ app.js
@@ -40,11 +40,12 @@ var sessionChecker = (req, res, next) => {
     }    
 };
 
-var bussessionChecker = (req, res, next) => {
-	if(!req.session.buses) {
-		res.redirect('/home');
+var checker = function(req,res,next){
+	if(!req.session.user){
+		res.redirect('/login');
 	}
-	else{
+	else
+	{
 		next();
 	}
 }
@@ -123,7 +124,6 @@ app.post('/register', urlencodedParser, function(req,res){
 
 app.get('/home', function(req,res){
 	var someVar = [];
-
 	database.connection.query("select * from STATION", function(err, rows){
 	  if(err) {
 	    throw err;
@@ -131,7 +131,13 @@ app.get('/home', function(req,res){
 	    someVar = rows;
 	    console.log();
 		var check = req.session.user;
-		res.render('home',{name: check, stations: someVar});
+		async function book(check){
+			var alpha = await database.connection.query('SELECT * FROM BOOKING_HISTORY WHERE username = ?', [check], function(err, rows, fields){
+				if(err) throw err;
+				res.render('home',{name: check, stations: someVar, rows: rows});
+			})
+		}
+		book(check);
 	  }
 	});
 });
@@ -163,6 +169,15 @@ app.post('/home', urlencodedParser, function(req,res){
 		}
 		var alpha = rows[0][0].stn_id;
 		var beta = rows[1][0].stn_id;
+		var one = await database.connection.query('SELECT distance FROM STATION WHERE stn_id = ?', [alpha]);
+		var two = await database.connection.query('SELECT distance FROM STATION WHERE stn_id = ?', [beta]);
+		two = two[0].distance;
+		one = one[0].distance;
+		var distance = two-one;
+		console.log("distance ", distance);
+		if(one>two)
+			distance = distance*-1;
+		req.session.distance = distance;
 		if(beta>alpha)
 		{
 			var bus = await database.connection.query('SELECT START.bus_id FROM START WHERE START.stn_id<=? AND START.bus_id IN (SELECT DESTINATION.bus_id FROM DESTINATION WHERE DESTINATION.stn_id>=?)', [alpha,beta]);
@@ -191,6 +206,9 @@ app.post('/home', urlencodedParser, function(req,res){
 			else
 			{
 				req.session.buses = bus;
+				req.session.day = day;
+				req.session.month = month;
+				req.session.year = year;
 				res.redirect('/buses');
 			}
 		}
@@ -205,14 +223,81 @@ app.get('/logout', function(req,res){
 	res.redirect('/home');
 });
 
-app.get('/buses', bussessionChecker, function(req,res){
+app.get('/buses', function(req,res){
+	if(!req.session.buses)
+		res.redirect('/home');
 	res.render('buses', {bus: req.session.buses, name: req.session.user});
 });
 
 app.post('/buses/:id', function(req,res){
-	console.log('welcome:');
-	console.log(req.params.id);
-	res.redirect('/buses');
+	//console.log(req.params.id);
+	req.session.busid = req.params.id;
+	console.log(req.params.id, req.session.busid);
+	res.redirect('/seats');
+});
+
+app.get('/seats', function(req, res){
+	if(!req.session.busid && req.session.buses) 
+		res.redirect('/buses');
+	if(!req.session.busid && !req.session.buses)
+		res.redirect('/home');
+	database.connection.query('SELECT ticket_id FROM BOOKING_HISTORY WHERE bus_id = ? AND day = ? AND month = ? AND year = ?', [req.session.busid, req.session.day, req.session.month, req.session.year], function(err, rows, fields){
+		if(err) throw err;
+		else
+		{
+			if(rows.length==30)
+			{
+				alert("Sorry all seats are full!");
+				res.redirect('/seats/:id');
+			}
+			var result = [];
+			for(var i=1; i<31; i++)
+			{
+				result.push(i);
+			}
+			for(var i=0; i<rows.length; i++)
+			{
+				var index = result.indexOf(rows[i].ticket_id);
+				if (index > -1) {
+				  result.splice(index, 1);
+				}
+			}
+			res.render('seats', {result: result, name: req.session.user});
+		}
+	});
+});
+
+app.post('/seats', checker, urlencodedParser, function(req, res){
+	var ticket = req.body.ticket;
+	console.log(req.session.busid);
+	database.connection.query('SELECT price_per_km FROM BUSES WHERE bus_id = ?', [req.session.busid], function(err, rows, fields){
+		var price_per_km = rows[0].price_per_km;
+		database.connection.query('SELECT wallet_balance FROM USERS WHERE username=?', [req.session.user], function(err,rows,fields){
+			console.log(req.session.user);
+			var wallet_balance = rows[0].wallet_balance;
+			var price = price_per_km*req.session.distance;
+			console.log(price_per_km, req.session.distance);
+			console.log(wallet_balance, price);
+			if(price>wallet_balance)
+			{
+				alert('Not enough money!');
+			}
+			else
+			{
+				async function haha(wallet_balance){
+					console.log("check");
+					for(var i=0; i<ticket.length; i++)
+					{
+						var x = await database.connection.query('INSERT INTO BOOKING_HISTORY VALUES (?,?,?,?,?,?)', [req.session.busid, req.session.day, req.session.month, req.session.year, ticket[i], req.session.user]);
+					}
+					console.log('this is so sad');
+					var y = await database.connection.query('UPDATE USERS SET wallet_balance = ? WHERE username = ?', [wallet_balance-price, req.session.user]);
+					res.redirect('/home');
+				}
+				haha(wallet_balance);
+			}
+		});
+	});
 });
 
 app.listen(8080);
