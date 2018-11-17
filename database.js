405ec4bc10669270f99519f6a98ae4dbdd159466
Devansh Shah <shahdevansh2000@gmail.com>
2018-11-17 18:27:31 +0530

    tryna run my app for everyone

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 var util = require('util');
 
 var connection = mysql.createPool({
-	host: 'localhost',
-	database: 'testdb',
-	user: 'root',
-	password: 'devansh123'
+	host: process.env.DB_HOST || 'localhost',
+	database: process.env.DB_NAME || 'testdb',
+	user: process.env.DB_USER || 'root',
+	password: process.env.DB_PASS || 'devansh123'
 });
 
 
